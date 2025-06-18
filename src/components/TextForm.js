bb4e989e6b12c9b63b1fbdc8b0afe393cdb1079c
Paresh Patil <patilparesh874@gmail.com>
2025-06-18 09:50:32 +0530

    Enhancing TextUtils

---
+++ src/components/TextForm.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("Uppercase was clicked: " + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
+  const handleClear = () => {
+    let newText = "";
+    setText(newText);
+  }
+
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState('');
+  // text = "new text";  // Wrong way to change the state
+  // setText("new text");  // Correct way to change the state
+  return (
+    <>
+      <div className="container">
+          <h1>{props.heading}</h1>
+          <div className="mb-3">
+              <textarea className="form-control" placeholder='Enter Your Text Here...' value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+          </div>
+          <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+          <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+          <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
+      </div>
+      <div className="container my-3">
+          <h2>Your text summary</h2>
+          <p>
+            {
+              text.trim().length === 0
+                ? 0
+                : text.trim().split(/\s+/).length
+            } words and {text.length} characters
+          </p>
+          <p>
+            {
+              text.trim().length === 0
+                ? 0
+                : (0.008 * text.trim().split(/\s+/).length).toFixed(2)
+            } Minutes to read
+          </p>
+          <h3>Preview</h3>
+          <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
+      </div>
+    </>
+  )
+}
